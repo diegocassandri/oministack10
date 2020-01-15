4d3adfa443e4a70ae66011ea020c984f230805ce
Diego Cassandri <diego_aox886@hotmail.com>
2020-01-15 03:27:32 +0000

    setup backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,8 @@ const app = express();
 
 mongoose.connect('mongodb+srv://oministack:oministack@oministack-kokfb.mongodb.net/week10?retryWrites=true&w=majority',{
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true ,
+    useCreateIndex: true
 },);
 
 app.use(express.json());
